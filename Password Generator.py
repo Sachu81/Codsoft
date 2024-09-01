5176bcb46e064bfdde5dca66c5319d8a308b07cf
Sachu81 <sachuchavan1137@gmail.com>
2024-09-01 15:00:53 +0530

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,15 @@
+#PASSWORD GENARATOR
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j', 'K', 'L', 'M', 'N', 'O', 'P', 'Q'
+           , 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+character = ['!', '@', '#', '$', '%', '&', '*', '+', ':', '<', '>', '/', '(', ')']
+
+# Give The Input From User
+numbers_letters = int(input("Enter The Number of letters Do You want:"))
+
