56e2dff32d2a931da05cb4c2a8a98a53d278c6ad
Sachu81 <sachuchavan1137@gmail.com>
2024-09-04 21:49:44 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+def add(x, y):
+    return x+y
+
+
+def sub(x, y):
+    return x-y
+
+
+def multi(x, y):
+    return x*y
+
+
+def div(x, y):
+    return x/y
+
+
+print("Select Operation")
+print(1, "+")
+print(2, '-')
+print(3, '*')
+print(4, '/')
+
