48f13d29370d1c5d157e96e3cb027164005345e0
Sachu81 <sachuchavan1137@gmail.com>
2024-09-07 19:57:02 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -21,3 +21,26 @@ print(2, '-')
 print(3, '*')
 print(4, '/')
 
+while True:
+    choice = input("Enter Choice('1','2','3','4'):")
+    if choice in ('1','2','3','4'):
+        try:
+            num1 = float(input("Enter First Number:")
+            num2 = float(input("Enter Second Number:")
+
+            except ValueError:
+                print("Invalid Input.")
+                continue
+            if choice == '1':
+                print(num1,"+",num2,"=",add(num1,num2))
+            if choice == '2':
+                print(num1,"-",num2,"=",sub(num1,num2))
+            if choice == '3':
+                print(num1,"*",num2,"=",multi(num1,num2))
+            if choice == '4':
+                print(num1,"/",num2,"=",div(num1,num2))
+            next_calculation = input("You Want Next Calculation? (yes/no):")
+            if next_calculation == 'no':
+                break
+            else:
+                print("Invakid Input")
